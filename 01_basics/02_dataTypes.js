78cb9d624ffc55354b3f7f8c1f39f32d5f35c79c
keshav-maheshwari707 <keshav.maheshwari707@gmail.com>
2024-08-12 20:31:49 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+let fullName = "Keshav"; // string
+let age = 19;            // number
+let isStudent = true;    // boolean
+let state;               // undefined
+let nullValue = null;    // null (its type is object)
+let fullName2 = ""       // string (not null)
+
+console.table([typeof fullName, typeof age, typeof isStudent, typeof state, typeof nullValue, typeof fullName2]);
+
+/*
+Primitive data types are = string, number, boolean, undefined, null, bigInt, symbol.
+Non-Primitive data type = object.
+*/

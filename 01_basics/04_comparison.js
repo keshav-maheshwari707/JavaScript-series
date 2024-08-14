0c8386e60fbd34ca28a73312aee5467764ae9c79
keshav-maheshwari707 <keshav.maheshwari707@gmail.com>
2024-08-14 19:17:33 +0530

    counter program

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,6 +1,21 @@
-console.log(10 > 5);
-console.log(10 >= 5);
-console.log(10 < 5);
-console.log(10 <= 5);
-console.log(10 == 5);
-console.log(10 != 5);
+// console.log(10 > 5);
+// console.log(10 >= 5);
+// console.log(10 < 5);
+// console.log(10 <= 5);
+// console.log(10 == 5);
+// console.log(10 != 5);
+
+// let age = window.prompt("how old are you?");
+// age = Number(age);
+// age += 1;
+// console.log(age);
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, y, z, typeof x, typeof y, typeof z);
